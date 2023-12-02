e3ead731165db609311aab9e6f48357f37913b6a
jeff <jjeffreywang@gmail.com>
2023-12-02 15:00:26 -0800

    add remove entries, better table formatting

--- app.js
+++ app.js
@@ -2,23 +2,81 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express()
+app.use(express.json());
 const portNumber = 80; // http port
 
 app.get('/budget', (req, res) => {
-    console.log("Budget Page reached");
+    console.log("=========== Main Budget Page has been reached ===========");
     fs.createReadStream("./pages/budget.html").pipe(res);
 });
 
 app.get('/budget.js', (req, res) => {
-    console.log("Budget JS Page reached");
+    console.log("_Budget.js is requested");
     fs.createReadStream("./scripts/budget.js").pipe(res);
 });
 
+app.get('/budget/categories.list', (req, res) => {
+    console.log("_budget category list has been requested");
+    fs.createReadStream("./scripts/categories.list").pipe(res);
+});
+
+app.get('/budget/data', (req, res) => {
+    console.log("_budget data information has been requested");
+    fs.stat("./data/budget_data.tsv", (err, stat) => {
+        if (err === null) {
+            fs.createReadStream("./data/budget_data.tsv").pipe(res);
+        } else {
+            res.end();
+        }
+    });
+});
+
 app.post('/budget', (req, res) => {
-    console.log("Budget Page SETTING reached");
-    res.end()
+    let budgetData = req.body.data;
+    fs.open("./data/budget_data.tsv", "a", (e, id) => {
+        fs.write(id, budgetData + "\r\n", null, 'utf8', () => {
+            fs.close(id, () => console.log(`_setting budgets data: ${budgetData}`));
+        });
+    });
+    res.end();
+});
+
+app.post('/budget/addcategory', (req, res) => {
+    let newCategory = req.body.data;
+    fs.open('./scripts/categories.list', 'a', (e, id) => {
+        fs.write(id, newCategory + "\r\n", null, 'utf8', () => {
+            fs.close(id, () => console.log(`_budget categories has been updated with value: ${newCategory}`));
+        });
+    });
+    res.end();
+});
+
+app.post('/budget/remove', (req, res) => {
+    let entryToRemove = req.body.data;
+    fs.readFile('./data/budget_data.tsv', 'utf8', (e, data) => {
+        if (e) {
+            console.log(`Removing data entry ${entryToRemove} resulted in failure`);
+        } else {
+            console.log(`_removing budget data entry: ${entryToRemove}`);
+            entryToRemove = entryToRemove.split("\t");
+            let newData = data.split(/\r?\n/).filter((entry) => {
+                entry = entry.split("\t");
+                for (let i = 0; i < entry.length; i+=1) {
+                    console.log(`_removingentryeawefawefaweftryentryentry: ${entryToRemove[i]}`);
+                    console.log(`_removingeytryentryentry: ${entry[i]}`);
+                    if (Number(entryToRemove[i]) && Number(entryToRemove[i]) !== Number(entry[i]) && entryToRemove[i] !== entry[i]) {
+                        return true
+                    }
+                }
+                return false;
+            });
+            newData = newData.join("\r\n");
+            fs.writeFile('./data/budget_data.tsv', newData, () => {});
+        }
+    });
+    res.end();
 });
 
 app.listen(portNumber, () => {
-    console.log(`Starting listening on port ${portNumber}`)
-})
+    console.log(`Starting listening on port ${portNumber}`);
+});
--- scripts/budget.js
+++ scripts/budget.js
@@ -1,40 +1,39 @@
-const categories = ["Utilities" ,
-                    "Rent" ,
-                    "Groceries" ,
-                    "Outside Food" ,
-                    "Gas & Car" ,
-                    "Entertainment event" ,
-                    "Entertainment item" ,
-                    "Pet Food" ,
-                    "Clothing" ,
-                    "Bathroom supplies"];
+let categories = [];
 
 const form = document.getElementById("budgetForm");
 const dateElement = document.getElementById('purchaseDate');
 const categoryElement = document.getElementById("category");
-const dummy = document.getElementById("dummy");
+const newCategoryElement = document.getElementById("newCategory");
+const newCategoryButtonElement = document.getElementById("newCategoryButton");
+const deleteModeElement = document.getElementById("deleteMode");
+const budgetTable = document.getElementById("budgetTable");
+const budgetTableBody = document.getElementById("budgetTable").tBodies[0];
 
 
-function budgetSubmit() {
+async function budgetSubmit() {
     let serializedInputValue = "";
     for (const inputElement of form.elements) {
-        serializedInputValue += inputElement.value + "\t";
+        if (!inputElement.className.includes("form_ignore")) {
+            serializedInputValue += inputElement.value + "\t";
+        }
     }
-
+    serializedInputValue = serializedInputValue.trim();
     if (checkSerializedValue(serializedInputValue)) {
-        dummy.innerHTML = serializedInputValue;
-        resetForm();
+        await fetch_post_helper("/budget", serializedInputValue);
 
+        resetForm();
+        await buildTable();
         return serializedInputValue;
     }
 };
 
 function resetForm() {
     form.reset();
+    refreshCategories();
     dateElement.valueAsDate = new Date();
 }
 
-function addCategoryOption(categoryText) {
+function addCategoryOptionElement(categoryText) {
     const newOption = document.createElement("option");
     newOption.value = categoryText;
     newOption.innerHTML = categoryText;
@@ -43,8 +42,7 @@ function addCategoryOption(categoryText) {
 }
 
 function checkSerializedValue(serializedValue) {
-    //const regexPattern = /^.+,\d+(\.\d{0,2})?,\d{4}-\d{2}-\d{2},.+,$/; // comma seperated
-    const regexPattern = /^.+\s\d+(\.\d{0,2})?\s\d{4}-\d{2}-\d{2}\s.+\s$/; // tab
+    const regexPattern = /^\w+\t\d+(\.\d{0,2})?\t[^\t]+\t\d{4}-\d{2}-\d{2}$/; // tab separated
     if (!regexPattern.test(serializedValue)) {
         alert("Please fill out form correctly");
         return false;
@@ -52,5 +50,83 @@ function checkSerializedValue(serializedValue) {
     return true;
 }
 
-resetForm();
-categories.forEach((category) => addCategoryOption(category));
+async function refreshCategories() {
+    categoryElement.innerHTML = "(<option value=\"\">-- Select --</option>)";
+
+    const categoriesFetch = await fetch("/budget/categories.list");
+    categories = await categoriesFetch.text();
+    categories = categories.trim().split(/\r?\n/);
+    categories.forEach((category) => addCategoryOptionElement(category));
+}
+
+function addNewCategory() {
+    let newCategory = newCategoryElement.value.trim();
+    newCategory = newCategory.charAt(0).toUpperCase() + newCategory.slice(1);
+
+    if (categories.includes(newCategory)) {
+        alert("Category already exists!");
+    } else if (newCategory !== "" && window.confirm(`Add new category ${newCategory}?`)) {
+        fetch_post_helper("/budget/addcategory", newCategory);
+        newCategoryElement.value = "";
+        resetForm();
+    }
+}
+
+async function fetch_post_helper(url, body) {
+    await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({data:body}),
+        headers: {'Content-type': 'application/json; charset=UTF-8'},
+    });
+}
+
+async function buildTable() {
+    let data = await fetch("/budget/data");
+    data = await data.text();
+    budgetTableBody.replaceChildren();
+    if (!data) {return false;}
+    data = data.trim().split(/\r?\n/);
+    data = data.map((entry) => entry.split("\t"));
+
+    for (entry of data) {
+        console.log(entry)
+        const newRow = document.createElement("tr");
+        newRow.onclick = dataRowOnClick;
+        for (elem of entry) {
+            const newEntry = document.createElement("td");
+            newEntry.innerHTML = elem;
+            newRow.appendChild(newEntry);
+        }
+        budgetTableBody.appendChild(newRow);
+    }
+
+    for (row of budgetTableBody.rows) { // format cost values to look better
+        const costCell = row.cells[1];
+        costValue = costCell.innerText;
+        let decimalIndex = costValue.indexOf(".");
+        if (decimalIndex >= 0) {
+            costCell.innerText = costValue.concat("0".repeat(decimalIndex-costValue.length+3));
+        } else {
+            costCell.innerText = costValue.concat(".00");
+        }
+    }
+}
+
+async function dataRowOnClick(event) {
+    const entryToRemove = event.currentTarget.innerText;
+    if (deleteModeElement.checked && confirm(`Delete entry ${entryToRemove}`)) {
+        await fetch_post_helper("/budget/remove", entryToRemove);
+        await buildTable();
+    }
+}
+
+function main() {
+    newCategoryButtonElement.onclick = (e) => {
+        addNewCategory(e);
+        return false; // stop propagating
+    };
+    resetForm();
+    buildTable();
+}
+
+main();
